65f419655c9dd3658c5a82ef3f6eb19ac1f79f63
Weverton <weverton.saluse@gmail.com>
2022-08-16 00:05:25 -0300

    Atualização de Portifólio sql

--- scripts/SQLQuery5.sql
+++ scripts/SQLQuery5.sql
@@ -1,1 +1,28 @@
 -- Operadores UNION / UNION ALL
+USE NORTHWND
+--Simulando Erro UNION
+
+SELECT 1
+UNION 
+SELECT 'A'
+
+-- EXEMPLO USANDO UNION
+
+	SELECT city
+	FROM Customers
+
+	UNION
+
+	SELECT city
+	FROM Suppliers
+	ORDER BY City
+-- EXEMPLO USANDO UNION ALL
+
+	SELECT city
+	FROM Customers
+
+	UNION ALL
+
+	SELECT city
+	FROM Suppliers
+	ORDER BY city
