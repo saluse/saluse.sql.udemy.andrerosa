4248b111700a1f7d71c4f5071751496cfc31fcc7
Weverton <weverton.saluse@gmail.com>
2022-08-17 20:13:48 -0300

    Atualização de Portifólio

--- scripts/SQLQuery7.sql
+++ scripts/SQLQuery7.sql
@@ -5,3 +5,17 @@
 -- DML UPDATE
 	UPDATE Funcionários SET SALARIO = '700'
 	WHERE ID = '23'
+-- Ou Aumento de 50% do valor no salário atual.
+	UPDATE Funcionários SET SALARIO = SALARIO*1.5
+	WHERE ID = '22'
+-- Redução do valor 
+	UPDATE Funcionários SET SALARIO = salario - SALARIO
+-- Exemplo de UPDATE com mais de um campo.
+	UPDATE Funcionários SET SALARIO = '1500'
+-- MEUS TESTES
+	UPDATE Funcionários SET SETOR = 'Manutenção'
+	WHERE ID = '29'
+	UPDATE Funcionários SET SETOR = 'Produção'	
+	WHERE ID = '24'
+-- EXEMPLO DE UPDATE COM MAIS DE 1 CAMPO
+	UPDATE Funcionários
