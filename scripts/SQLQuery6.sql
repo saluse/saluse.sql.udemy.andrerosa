8b6fe3286ad56d0b148fdba547dc3b5255ed1da9
Weverton <weverton.saluse@gmail.com>
2022-08-16 23:00:34 -0300

    Atualização de Portifólio

--- scripts/SQLQuery6.sql
+++ scripts/SQLQuery6.sql
@@ -13,6 +13,28 @@ USE NORTHWND
 	FROM Customers
 		WHERE Country = 'Germany'
 	ORDER BY City
--- FIM
-
-		
+-- AULA 032
+-- DDL Criação de tabela para Exercicios.
+CREATE TABLE Funcionários
+(
+	ID INT IDENTITY(1,1),
+	NOME Varchar(50) NOT NULL,
+	SALARIO DECIMAL(10,2),
+	SETOR Varchar(30)
+	)
+-- DML SELECT
+-- EXEMPLO SELECT
+	SELECT * FROM Funcionários
+-- EXEMPLO SELECT
+	SELECT nome, setor
+	FROM Funcionários
+-- EXEMPLO SELECT
+	SELECT nome, setor AS depto -- AS serve para dar apelidos aos campos e facilitar 
+	FROM Funcionários
+-- DML INSERT
+	INSERT INTO Funcionários VALUES ('João',1000,'Vendas'), ('José',2000,'Atendimento'), ('Karla',3000,'Setor Pessoal')
+-- OU
+	INSERT INTO Funcionários (nome,salario) VALUES ('Pedro',1500)
+	
+-- DELETAR A TABELA
+	DELETE Funcionários
