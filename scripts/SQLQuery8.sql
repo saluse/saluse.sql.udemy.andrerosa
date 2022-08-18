7f5fd2fa850e1375e0c2fe3c81be4a632ec1cbf5
Weverton <weverton.saluse@gmail.com>
2022-08-17 23:00:23 -0300

    Atualização de Portifólio

---
+++ scripts/SQLQuery8.sql
@@ -0,0 +1,21 @@
+-- Aula 034
+-- DDL Criando Tabela
+USE Curso
+	CREATE TABLE FuncionáriosAju
+(			Matrícula	INT IDENTITY(1,1),
+			Nome		CHAR(50) NOT NULL,
+			Sobrenome	CHAR(50) NOT NULL,
+			Endereço	CHAR(50),
+			Cidade		CHAR(50),
+			País		CHAR(25),
+			Data_Nasc	DATETIME
+			)
+	SELECT * FROM FuncionáriosAju
+
+-- DDL Criando tabela com chave estrangeira.
+	CREATE TABLE Salário
+(
+	Matrícula INT PRIMARY KEY NOT NULL,
+	Salario		DECIMAL(10,2) NOT NULL,
+	FOREIGN KEY(Matrícula) REFERENCES funcionário(Matrícula)
+	)
