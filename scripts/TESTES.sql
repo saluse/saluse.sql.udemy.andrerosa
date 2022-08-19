4a33867e0d08a8e29a1285e1af0c95fe4f78828b
Weverton <weverton.saluse@gmail.com>
2022-08-18 23:52:07 -0300

    Atualização de Portifólio

---
+++ scripts/TESTES.sql
@@ -0,0 +1,8 @@
+SELECT * FROM FuncionáriosAju
+
+SELECT * FROM Funcionário
+
+SELECT * FROM Colaborador
+
+INSERT INTO Colaborador VALUES ('Tom', 'Seixas', 'Rua Esperança', 'Aracaju', 'Brasil', 1987-12-20)
+INSERT INTO Colaborador VALUES ('Karla', 'Seixas', 'TV Esperança', 'Aracaju', 'Brasil', 1992-07-26)
