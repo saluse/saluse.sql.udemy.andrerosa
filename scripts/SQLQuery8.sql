384bd515047c8e89dcf2c406077c53430399510e
Weverton <weverton.saluse@gmail.com>
2022-08-20 22:26:35 -0300

    Atualização de Portifólio

--- scripts/SQLQuery8.sql
+++ scripts/SQLQuery8.sql
@@ -11,12 +11,33 @@ USE Curso
 			Data_Nasc	DATETIME
 			)
 	SELECT * FROM FuncionáriosAju
-	SELECT * FROM Salario
--- DDL Criando tabela com chave estrangeira.
-USE Curso
-	CREATE TABLE Salário
+	SELECT * FROM audit_salario
+-- DDL CRIANDO TABELA COM CHAVE ESTRANGEIRA.
+
+CREATE TABLE Salario
 (
-	Matrícula INT PRIMARY KEY NOT NULL,
-	Salario		DECIMAL(10,2) NOT NULL,
-	FOREIGN KEY(Matrícula) REFERENCES funcionário(Matrícula)
+	matrícula INT PRIMARY KEY NOT NULL,
+	salario		DECIMAL(10,2) NOT NULL
+	FOREIGN KEY(Matrícula) 	REFERENCES salario(Matrícula)
 	)
+SELECT * FROM Salario
+-- AULA 035
+-- DDL CRIANDO TABELA COM CHAVE PRIMARIA
+CREATE TABLE audit_salario
+	(
+		transação INT IDENTITY(1,1),
+		matricula INT NOT NULL,
+		data_trans DATETIME NOT NULL,
+		sal_antigo DECIMAL(10,2),
+		sal_novo DECIMAL(10,2),
+		usuário VARCHAR(20) NOT NULL	
+		)
+ SELECT * FROM audit_salario
+
+ -- DDL CRIAÇÂO DE INDEX
+
+ CREATE INDEX ix_func1	ON funcionário(data_nasc)
+ CREATE INDEX ix_func2	ON funcionário(Cidade, país)
+ 
+ -- ADICIONAR NOVO CAMPO NA TABELA
+ ALTER TABLE Funcionário ADD genero CHAR(1);
