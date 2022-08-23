1b3afb1a2ceeeae3c48bf345b83245a25c721f56
Weverton <weverton.saluse@gmail.com>
2022-08-22 21:30:35 -0300

    Atualização de Portifólio

--- scripts/SQLQuery9.sql
+++ scripts/SQLQuery9.sql
@@ -3,3 +3,11 @@
 /* Mantém os usúarios de permissão de leitura / gravação em objetos.*/
 -- PARTE 01 CONCEDE - GRANT
 -- CRIA UM LOGIN E DÁ PERMISSÂO NO BANCO.
+EXEC master.dbo.sp_addlogin 'UsrTeste','SenhaTeste'
+
+-- Concedendo acesso de ATUALIZAÇÃO PARA UsrTeste.
+GRANT UPDATE ON Funcionário TO UsrTeste;
+
+-- Concedendo acesso de INSERT para UsrTeste.
+GRANT INSERT ON Funcionário TO UsrTeste;
+SELECT * from UsrTeste
