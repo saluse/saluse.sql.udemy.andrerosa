34058c06afb847e920d46ff1950900e60e6474ac
Weverton <weverton.saluse@gmail.com>
2022-08-26 20:35:43 -0300

    Atualização de Portifólio

---
+++ scripts/SQLQuery12.sql
@@ -0,0 +1,32 @@
+-- AULA 042
+-- CONTINUAÇÃO
+-- Clientes que fizeram pedidos antes da data atual
+SELECT Cod_CLI,
+	   Cli_Nomes
+FROM   Clientes
+WHERE  Cod_cli IN (SELECT Cod_CLI
+			FROM Pedidos
+			WHERE Data < Getdate())
+
+SELECT GETDATE()
+
+SELECT * FROM Clientes
+
+-- AULA 043
+-- Clientes que já fizeram algum pedido
+SELECT p.Num_ped,
+	   p.data,
+	   p.cod_cliente,
+	   (SELECT c.Cli_Nomes
+	   FROM Clientes C
+	   WHERE p.cod_Cliente = c.Cod_CLI) AS Nome_CLI
+FROM Pedidos AS P
+
+-- Query com Subquery traz o total de cada cliente partindo da tabela Pedidos.
+SELECT P.Cod_Cliente,
+	   (SELECT c.cli_nomes
+	   FROM Clientes C
+	   WHERE p.cod_cliente = c.Cod_CLI) AS Cli_nomes,
+	   SUM(p.total) Total
+FROM Pedidos AS P
+GROUP BY p.Cod_Cliente
