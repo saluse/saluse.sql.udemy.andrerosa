8f54f228b6fa87d67ed3a3ffa1a4f1a911b875f1
Weverton <weverton.saluse@gmail.com>
2022-08-30 18:49:52 -0300

    - Atualização de Portifólio

--- scripts/SQLQuery13.sql
+++ scripts/SQLQuery13.sql
@@ -77,7 +77,7 @@ INSERT INTO Matricula VALUES ('5','1','Matutino')
 INSERT INTO Matricula VALUES ('5','2','Vespertino')
 INSERT INTO Matricula VALUES ('5','4','Noturno')
 
--- AULA 045 E 046
+-- AULA 045
 -- ALUNO CODIGO 4 NÃO TEM MATRICULA
 -- DISCIPLINA 5 NÃO TEM ALUNOS
 
