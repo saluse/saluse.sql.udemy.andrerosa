5aaba69dd0536385646554ed47ea1c455efbc2a1
Weverton <weverton.saluse@gmail.com>
2022-08-31 16:45:49 -0300

    Atualização de POrtifólio

--- scripts/TESTES.sql
+++ scripts/TESTES.sql
@@ -16,3 +16,33 @@ USE Curso
 			)
 
 SELECT * FROM Salários
+
+CREATE DATABASE Hashtag
+
+USE Hashtag
+CREATE TABLE Vendas
+(
+	ID_Venda INT,
+	Curso Varchar(100),
+	Aluno Varchar(100),
+	Estado Varchar(50),
+	Valor Decimal(10,2)
+	)
+INSERT INTO Vendas VALUES(1,'Excel','João','SP',100)
+INSERT INTO Vendas VALUES(2,'VBA','Lucas','RJ',150)
+INSERT INTO Vendas VALUES(3,'Excel','Alice','SP',100)
+INSERT INTO Vendas VALUES(4,'Excel','Pedro','PE',100)
+
+INSERT INTO Vendas VALUES(5,'VBA','Amanda','RS',150)
+INSERT INTO Vendas VALUES(6,'BI','Rita','SE',120)
+
+INSERT INTO Vendas VALUES(7,'Excel','Julia','BA',100)
+INSERT INTO Vendas VALUES(8,'BI','Caio','MG',120)
+INSERT INTO Vendas VALUES(9,'BI','Lara','AC',120)
+INSERT INTO Vendas VALUES(10,'Excel','Rogério','SE',100)
+
+SELECT * FROM Vendas
+SELECT Aluno, Curso, Valor FROM Vendas
+
+
+		
