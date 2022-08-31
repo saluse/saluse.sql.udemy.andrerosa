568b74947a5ac513b1a76df47e6612d46c3a3485
Weverton <weverton.saluse@gmail.com>
2022-08-31 15:45:20 -0300

    Atualização de Portifólio

---
+++ scripts/SQLQuery14.sql
@@ -0,0 +1,9 @@
+-- AULA 046
+-- CONHECENDO AS TABELAS
+USE Curso
+SELECT TOP 1 * FROM senso_2013
+SELECT TOP 1 * FROM Cidades
+SELECT * FROM Uf
+SELECT * FROM Regiao_Uf
+
+-- AVG Retorna a média dos valores em um grupo. Valores Nulos são ignorados.
