d1910ac91489bf4f6a1d0e766cf1fe76c5222859
Weverton <weverton.saluse@gmail.com>
2022-08-31 15:49:58 -0300

    Atualização de Portifólio

--- scripts/SQLQuery14.sql
+++ scripts/SQLQuery14.sql
@@ -7,3 +7,10 @@ SELECT * FROM Uf
 SELECT * FROM Regiao_Uf
 
 -- AVG Retorna a média dos valores em um grupo. Valores Nulos são ignorados.
+
+SELECT AVG(Populacao) FROM Cidades
+
+-- AVG MÉDIA POR ESTADO
+SELECT UF, AVG(Populacao) FROM Cidades
+GROUP BY Uf
+ORDER BY 2
