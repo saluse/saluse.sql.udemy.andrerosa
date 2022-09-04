b9787e5115b4264474fa0823e8d773150b192c25
Weverton <weverton.saluse@gmail.com>
2022-09-04 15:34:00 -0300

    Comando UPDATE

--- scripts/SQLQuery7.sql
+++ scripts/SQLQuery7.sql
@@ -2,7 +2,9 @@
 	SELECT * FROM Funcionários		
 -- SIMULANDO ERRO
 	INSERT INTO Funcionários (nome, SALARIO) VALUES ('Pedro','asaas')
+
 -- DML UPDATE
+
 	UPDATE Funcionários SET SALARIO = '700'
 	WHERE ID = '23'
 -- Ou Aumento de 50% do valor no salário atual.
