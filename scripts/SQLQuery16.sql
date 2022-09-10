fc9b84906495695e11008838d2990ccb2fb9044f
Weverton <weverton.saluse@gmail.com>
2022-09-09 22:13:51 -0300

    AULA 052 - Atualização

--- scripts/SQLQuery16.sql
+++ scripts/SQLQuery16.sql
@@ -9,3 +9,15 @@ Use AdventureWorks2014
  SELECT productcategoryid,     
 Choose (productcategoryid, 'A', 'B', 'C', 'D', 'E') AS Expressao 
 FROM   production.productcategory;
+
+--EXEMPLO 03
+
+ SELECT jobtitle,        
+        hiredate,
+		Month(hiredate)mes,        
+		Choose(Month(hiredate), 'Winter', 'Winter', 'Spring', 'Spring', 
+		                        'Spring','Summer', 'Summer','Summer', 
+								'Autumn', 'Autumn', 'Autumn','Winter') AS Quarter_Hired 
+		FROM   humanresources.employee 
+		WHERE  Year(hiredate) > 2005 
+		ORDER  BY Year(hiredate);
