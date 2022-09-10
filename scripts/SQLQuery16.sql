865f6a916bb8d787b41ffea59c9c79e5e7cb2c5a
Weverton <weverton.saluse@gmail.com>
2022-09-09 22:14:23 -0300

    Atualização de Portifólio

--- scripts/SQLQuery16.sql
+++ scripts/SQLQuery16.sql
@@ -1,10 +1,10 @@
 -- AULA 052
 --CHOOSE
 --RETORNA O ITEM AO INDICE ESPECIFICADO DE UMA LISTA DE VALORES NO SQL SERVER.
---EXEMPLO 1
+--EXEMPLO 01
 SELECT Choose (3, 'Gerente', 'Diretor', 'Desenvolvedor', 'Tester') AS Escolhido 
 
---EXEMPLO 2
+--EXEMPLO 02
 Use AdventureWorks2014
  SELECT productcategoryid,     
 Choose (productcategoryid, 'A', 'B', 'C', 'D', 'E') AS Expressao 
