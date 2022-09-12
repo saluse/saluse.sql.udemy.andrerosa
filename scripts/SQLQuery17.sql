2b926e39c07c4201ee8d47e939f27fb3617314f8
Weverton <weverton.saluse@gmail.com>
2022-09-11 22:32:03 -0300

    AULA 054 - TOP 10

--- scripts/SQLQuery17.sql
+++ scripts/SQLQuery17.sql
@@ -5,3 +5,16 @@ SELECT TOP 3 * FROM customers
 
 SELECT TOP 10 a.CustomerID,a.CompanyName 
 FROM customers a
+
+--10 CLIENTES COM O MAIOR VOLUME DE PEDIDO (COMPRAS)
+USE NORTHWND
+SELECT TOP 10
+	RANK() OVER(ORDER BY SUM(c.Quantity*c.UnitPrice) DESC)Posicao,
+       A.CompanyName,
+	   SUM(c.Quantity*c.UnitPrice) Tot_compras
+	FROM Customers a
+	INNER JOIN Orders b
+	ON a.CustomerID=b.CustomerID
+	INNER JOIN [Order Details] c
+  ON b.OrderID=c.OrderID
+  GROUP BY a.CompanyName
