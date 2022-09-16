9784556f9bb4c722c7367b255babc714b858979c
Weverton <weverton.saluse@gmail.com>
2022-09-15 23:46:31 -0300

    AULA 055 - Convert

--- scripts/SQLQuery18.sql
+++ scripts/SQLQuery18.sql
@@ -17,7 +17,7 @@ SELECT CONVERT(DECIMAL(5, 2), CONVERT(VARBINARY(20), @meuvalor));
     FROM   production.product 
 	WHERE  CAST(listprice AS INT) LIKE '3%';
 
--- Use CONVERT.   
+-- USE CONVERT.   
 	SELECT Substring(NAME, 1, 30) AS ProductName,        
 	listprice 
 	FROM   production.product 
