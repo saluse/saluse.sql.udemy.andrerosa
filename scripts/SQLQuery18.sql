5c354f73833ae3d99818382627bfbb979727731a
Weverton <weverton.saluse@gmail.com>
2022-09-25 23:31:46 -0300

    Atualização de Portifólio

--- scripts/SQLQuery18.sql
+++ scripts/SQLQuery18.sql
@@ -7,6 +7,7 @@ SET @meuvalor = 193.57; 
 SELECT Cast(@meuvalor AS VARBINARY(20)); 
 SELECT Cast(Cast(@meuvalor AS VARBINARY(20)) AS DECIMAL(5, 2)); 
 
+
 -- OR, USING CONVERT  
 SELECT CONVERT(VARBINARY(20), @meuvalor); 
 SELECT CONVERT(DECIMAL(5, 2), CONVERT(VARBINARY(20), @meuvalor));
